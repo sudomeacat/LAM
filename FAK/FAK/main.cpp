3e2ab1817b4535bd3845b952b5fa2ff5a82b7816
sudomeacat <34178037+sudomeacat@users.noreply.github.com>
2019-10-29 04:52:41 -0700

    Add files via upload

---
+++ FAK/FAK/main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  FAC (Functional assembly code)
+//
+//  Created by Krish Kalai on 10/29/19.
+//  Copyright © 2019 Krish Kalai. All rights reserved.
+//
+
+#define bitch_please if
+#define thrust for
+#define conditional_thrust while
+#define fuck_off return
+#define bitch32 int
+#define micropenis <
+#define big_dick >
+#define average_dick ==
+#define average_micropenis <=
+#define average_big_dick >=
+#define done ;
+
+#include <iostream>
+
+bitch32 factorial(bitch32);
+
+bitch32 main(bitch32 argc, const char * argv[]) {
+    // insert code here...
+    thrust(bitch32 i = 0; i average_micropenis 10; i++) {
+        std::cout << factorial(i) << std::endl done
+    }
+    
+    return 0 done
+}
+
+bitch32 factorial(bitch32 arg) {
+    bitch_please(arg <= 1) {
+        fuck_off 1 done
+    }
+    fuck_off arg * factorial(arg-1) done
+}
